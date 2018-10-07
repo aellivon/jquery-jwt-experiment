1794f3e48d6491e9401be932cc50aab96f72cbb6
aellivon <aellivon@gmail.com>
2018-10-07 22:46:35 +0800

    .gitignore environment

--- sample_jwt_server/sample_jwt_server_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/aellivon/Projects/SunnyProjects/KwenReferences/sample_jwt_server_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
